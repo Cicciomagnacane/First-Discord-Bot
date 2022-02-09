b1fa296785920d01c75db6b801f448ce65e569ea
Francesco Oliveto <44893172+Cicciomagnacane@users.noreply.github.com>
2022-02-09 11:46:22 +0100

    Upload files

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require("Discord.js")
+const client =new Discord.Client(
+    {intents:["GUILDS", "GUILD_MEMBERS", "GUILD_MESSAGES"]}
+)
+
+client.login("OTQwNzI0MTM3MjU5ODU5OTg4.YgLjoA.Kjs9PpqKcOrmpYNb2SPAGBANtbE")
+
+client.on("ready", () => {
+    console.log("BOT ONLINE")
+})
+
+client.on("messageCreate", (message) => {
+    if(message.content=="!Twitch" || message.content=="!twitch")
+    {
+        message.channel.send("Seguitemi su Twitch: https://www.twitch.tv/magnacane")
+    }
+
+    if(message.content=="!embed")
+    {
+        var embed = new Discord.MessageEmbed()
+            .setTitle("Embed title")
+            .setDescription(`${message.author.username}`) 
+            .setThumbnail("https://play-lh.googleusercontent.com/xQ-meXSBylIU8VKA7yUQXDwRu99JX8ic7mAsM4sBidjRgtMyhBDmYD4CpATqrdc1SA")
+
+            message.channel.send({ embeds: [embed] })
+    }
+})
