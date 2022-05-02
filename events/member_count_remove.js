cdcff3393c57094580f7f9f34dd0f4108d8da3f9
Francesco Oliveto <44893172+Cicciomagnacane@users.noreply.github.com>
2022-05-02 10:03:28 +0200

    fix

--- events/member_count_remove.js
+++ events/member_count_remove.js
@@ -3,7 +3,7 @@ module.exports = {
     type: "discord",
     execute(member) 
     {
-        var canale= client.channels.cache.get("941281278257401936")
+        var canale= client.channels.cache.get("969308875134214175")
         canale.setName("Members: " + member.guild.memberCount)
     }
 }
